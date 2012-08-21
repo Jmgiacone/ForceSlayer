6ae80d10be8343cd29bc9c03ca09306aa9dbb3f2
Nolan Hoffmann <9oxsteelflux@gmail.com>
2012-08-20 22:57:15 -0500

    GUI changes
    Added combat options

--- src/Slayer/InitGameDialog.java
+++ src/Slayer/InitGameDialog.java
@@ -12,8 +12,17 @@ import java.awt.GridLayout;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JRadioButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
 
 public class InitGameDialog extends JDialog {
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -32,6 +41,7 @@ public class InitGameDialog extends JDialog {
 	 * Create the dialog.
 	 */
 	public InitGameDialog() {
+		setTitle("Force Slayer v0.01");
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
 		
@@ -53,5 +63,91 @@ public class InitGameDialog extends JDialog {
 		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Combat Options", null, panel_1, null);
+		
+		JRadioButton rdbtnUseSafespots = new JRadioButton("Use safespots");
+		
+		JRadioButton rdbtnUseMagic = new JRadioButton("Use magic");
+		
+		JRadioButton rdbtnUseRange = new JRadioButton("Use range");
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"Strength potion", "Super strength potion", "Extreme strength potion"}));
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JLabel lblAmount = new JLabel("Amount:");
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"Attack potion", "Super attack potion", "Extreme attack potion"}));
+		
+		JLabel lblAmount_1 = new JLabel("Amount:");
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		
+		JComboBox comboBox_3 = new JComboBox();
+		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"Defense potion", "Super defense potion", "Extreme defense potion"}));
+		
+		JLabel lblAmount_2 = new JLabel("Amount:");
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1.setHorizontalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(rdbtnUseMagic)
+						.addComponent(rdbtnUseRange)
+						.addComponent(rdbtnUseSafespots)
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addComponent(comboBox_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lblAmount)
+							.addGap(4)
+							.addComponent(textField, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addComponent(comboBox_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lblAmount_1)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(textField_1, 0, 0, Short.MAX_VALUE))
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addComponent(comboBox_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lblAmount_2)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(textField_2, 0, 0, Short.MAX_VALUE)))
+					.addContainerGap(191, Short.MAX_VALUE))
+		);
+		gl_panel_1.setVerticalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(rdbtnUseMagic)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(rdbtnUseRange)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(rdbtnUseSafespots)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(comboBox_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblAmount)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(comboBox_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblAmount_1)
+						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(comboBox_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblAmount_2)
+						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(84, Short.MAX_VALUE))
+		);
+		panel_1.setLayout(gl_panel_1);
 	}
 }
