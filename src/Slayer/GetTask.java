27b548064fb891bd453359e877e20a2b6e7a3ce9
Jordan Giacone <jmgiacone@comcast.net>
2012-08-22 02:10:23 -0500

    Parameterized GetTask
    This allows us to easily pass in the Slayer Master for ease of use

--- src/Slayer/GetTask.java
+++ src/Slayer/GetTask.java
@@ -3,17 +3,18 @@ package Slayer;
 import org.powerbot.concurrent.Task;
 import org.powerbot.concurrent.strategy.Strategy;
 import org.powerbot.game.api.ActiveScript;
+import org.powerbot.game.api.methods.interactive.NPCs;
 import org.powerbot.game.api.wrappers.interactive.NPC;
 
 public class GetTask extends Strategy implements Task {
 
-	public NPC getNPC(NPC master) {
-		return master;
+	NPC master;
+	public GetTask(String master) {
+		this.master = NPCs.getNearest(master);
 	}
 
 	@Override
 	public void run() {
-		getNPC(null);
 		
 	}
 }
