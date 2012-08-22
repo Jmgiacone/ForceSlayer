74bbdac43ddca39c93f85a1a1c3161f6c098112b
Jordan Giacone <jmgiacone@comcast.net>
2012-08-22 00:51:11 -0500

    Fixed Nolan's retardation

--- src/Slayer/GetTask.java
+++ src/Slayer/GetTask.java
@@ -1,16 +1,19 @@
 package Slayer;
 
+import org.powerbot.concurrent.Task;
+import org.powerbot.concurrent.strategy.Strategy;
 import org.powerbot.game.api.ActiveScript;
 import org.powerbot.game.api.wrappers.interactive.NPC;
 
-public class GetTask extends ActiveScript {
+public class GetTask extends Strategy implements Task {
 
-	@Override
-	protected void setup() {
-		getNPC(null);
-	}
-	
 	public NPC getNPC(NPC master) {
 		return master;
+	}
+
+	@Override
+	public void run() {
+		getNPC(null);
+		
 	}
 }
